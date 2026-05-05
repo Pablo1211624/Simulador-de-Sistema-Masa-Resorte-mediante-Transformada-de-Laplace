b5ea20fbca09cd89af9702880a657cbe84cd3938
Pablo1211624 <pjgonzalezp@correo.url.edu.gt>
2026-05-04 23:06:19 -0600

    version no finalizada

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ from resolver_laplace import resolver_laplace
 
 
 def main():
-    #hacer el main
+    #unificar los modulos en el main
+    m = 1
+    c = 0
+    k = 4
+    x0 = 1
+    xp0 = 0
+
+    Xt = resolver_laplace(m, c, k, x0, xp0)
+    print("x(t) =", Xt)
 
 if __name__ == "__main__":
     main ()
--- resolver_laplace.py
+++ resolver_laplace.py
@@ -4,7 +4,7 @@ def resolver_laplace(m, c, k, x0, xp0):
     # definimos las variables independietes
     t, s = sp.symbols('t s')
 
-    numerador = m(s*x0 + xp0) + c*x0
+    numerador = m*(s*x0 + xp0) + c*x0
     denominador = m*s**2 + c*s + k
 
     #dejamos todo en funcion de s
@@ -12,5 +12,4 @@ def resolver_laplace(m, c, k, x0, xp0):
 
     #transformada inversa de laplace
     Xt = sp.inverse_laplace_transform(Xs, s, t)
-
     return Xt
